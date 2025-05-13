6f9c6d692dc4d6568cf13fc10e107f2b6010a46f
muhammadaliAtk <alinoorhussainsami11@gmail.com>
2025-05-13 22:19:30 +0500

    add auth component

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,17 @@ import React from 'react'
 import {StreamChat} from 'stream-chat';
 import {Chat} from 'stream-chat-react';
 import Cookies from 'universal-cookie';
-import {ChannelListContainer, ChannelContainer} from "./components"
+import {ChannelListContainer, ChannelContainer, Auth} from "./components"
 import "./App.css"
 const apiKey = 'qkuf5pmfaps4';
 const client = StreamChat.getInstance(apiKey)
+
+const authToken = false;
+
 const App = () => {
+  if(!authToken){
+    return <Auth/>
+  }
   return (
     <div className='app_wrapper'>
         <Chat client={client} theme='team light'>
